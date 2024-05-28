cdb99cabbcfdf74b68be76def2980a01f508de31
Vraj-Raval <ravalvraj3000@gmail.com>
2024-05-29 00:03:55 +0530

    first commit

---
+++ middleware/verifyToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../error.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return next(createError(401, "You are not authenticated!"));
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) return next(createError(401, "You are not authenticated!"));
+    const decode = jwt.verify(token, process.env.JWT);
+    req.user = decode;
+    return next();
+  } catch (err) {
+    next(err);
+  }
+};
